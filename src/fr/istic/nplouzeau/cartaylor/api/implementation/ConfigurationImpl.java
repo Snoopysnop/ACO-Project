f8cd28ef26dc8dbb83b3b8d42b501db7462ab6a7
Gombert Gwenael <gwenael.gombert@etudiant.univ-rennes1.fr>
2022-12-12 01:14:24 +0000

    Modif : constructeur pour etre en accord avec confiurator fonction isValid fonction isComplete fonction getSelectedParts + diverses optis

--- src/fr/istic/nplouzeau/cartaylor/api/implementation/ConfigurationImpl.java
+++ src/fr/istic/nplouzeau/cartaylor/api/implementation/ConfigurationImpl.java
@@ -1,7 +1,5 @@
 package fr.istic.nplouzeau.cartaylor.api.implementation;
 
-
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -13,16 +11,16 @@ public class ConfigurationImpl implements Configuration {
 
 	private Set<PartType> config = new HashSet<PartType>();
 	
-	private CompatibilityChecker checker;
+	private ConfiguratorImpl configurator;
+	private  CompatibilityChecker checker;
 	
-	public ConfigurationImpl(CompatibilityChecker checker, Set<PartType> config) {
-		this.checker=checker;
-		this.config=config;
+	public ConfigurationImpl(ConfiguratorImpl configurator) {
+		this.configurator=configurator;				
 	}
     public boolean isValid() {
+    	checker=configurator.getCompatibilityChecker();
     	for(PartType piece: config) {
     		Set<PartType> listIncompatibilities = checker.getIncompatibilities(piece);
-    		Set<PartType> listRequierements = checker.getRequirements(piece);
     		if(!listIncompatibilities.isEmpty()){
     			for(PartType p: config) {
     				if(p!=piece) {
@@ -30,6 +28,7 @@ public class ConfigurationImpl implements Configuration {
     				}	
     			}
     		}
+    		Set<PartType> listRequierements = checker.getRequirements(piece);
     		if(!listRequierements.isEmpty()) {
     			for(PartType p: listRequierements) {
     				if(p!=piece) {
@@ -43,39 +42,21 @@ public class ConfigurationImpl implements Configuration {
 
     
     public boolean isComplete() {
-    	//modif s'arreter des que compteurX>1;
-    	int compteurEngine=0;
-    	int compteurTransmission=0;
-    	int compteurExterior=0;
-    	int compteurInterior=0;
-    	    	
-    	for(PartType piece: config) {
-    		switch(piece.getCategory().getName()) {
-	    		case"Engine":
-	    			compteurEngine++;
-	    			break;
-	    		case"Transmission":
-	    			compteurTransmission++;
-	    			break;
-	    		case"Exterior":
-	    			compteurExterior++;
-	    			break;
-	    		case"Interior":
-	    			compteurInterior++;
-	    			break;
-	    		default:
-	    			
-    		}
-    	}
-    	return (compteurEngine==1)&&(compteurTransmission==1)&&(compteurExterior==1)&&(compteurInterior==1);
+    	
+    	return config.size()==configurator.getCategories().size();
+    	
     }
 
     public Set<PartType> getSelectedParts(){
     	return Collections.unmodifiableSet(config);
+    	//return config;
     }
 
     public void selectPart(PartType chosenPart) {
-    	config.add(chosenPart);
+    	if(!chosenPart.equals(null)) {
+    		config.add(chosenPart);
+    	}
+    	
     	
     }
 
@@ -95,14 +76,9 @@ public class ConfigurationImpl implements Configuration {
     	}
     	config=configRes;
     }
-
+    @Override
     public void clear() {//enlever toutes les pieces de la config
     	config = new HashSet<PartType>();
     	//config.clear();
     }
-    
-    public void addConfig(PartType p) {
-    	config.add(p);
-    }
-
 }
