fa48ed22e858bdaa0523dda31ad5ddb1c1ccc4f7
Gombert Gwenael <gwenael.gombert@etudiant.univ-rennes1.fr>
2022-12-19 17:06:34 +0000

    creation d'une classe de test pour CategoryImpl en testant les fonctions de CategoryImpl

--- src/test/CategoryImplTest.java
+++ src/test/CategoryImplTest.java
@@ -1,1 +1,25 @@
+package test;
+import static org.junit.jupiter.api.Assertions.*;
 
+
+import fr.istic.nplouzeau.cartaylor.api.implementation.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CategoryImplTest {
+
+	CategoryImpl categoryTransmission; 
+	
+	@BeforeEach
+	public void setUp() {
+		categoryTransmission = new CategoryImpl("Transmission");
+		
+	}
+	@DisplayName("Test du getName")
+	@Test
+	void getName1() {
+		assertEquals(categoryTransmission.getName(),"Transmission");
+	}
+}
