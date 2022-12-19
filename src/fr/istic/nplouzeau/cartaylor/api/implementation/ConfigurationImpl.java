a9f035496a35de135bbb2d8d71a657db5280a8f3
Gombert Gwenael <gwenael.gombert@etudiant.univ-rennes1.fr>
2022-12-19 16:36:25 +0000

    Update ConfigurationImpl.java : maj de toutes les fonctions qui font ce qu'elles sont censé faire

--- src/fr/istic/nplouzeau/cartaylor/api/implementation/ConfigurationImpl.java
+++ src/fr/istic/nplouzeau/cartaylor/api/implementation/ConfigurationImpl.java
@@ -1,11 +1,8 @@
 package fr.istic.nplouzeau.cartaylor.api.implementation;
 
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import fr.istic.nplouzeau.cartaylor.api.*;
+
 
 
 
@@ -13,26 +10,33 @@ public class ConfigurationImpl implements Configuration {
 
 	private Set<PartType> config = new HashSet<PartType>();
 	
-	private CompatibilityChecker checker;
+	private ConfiguratorImpl configurator;
+	private  CompatibilityChecker checker;
 	
-	public ConfigurationImpl(CompatibilityChecker checker) {
-		this.checker=checker;
+	public ConfigurationImpl(ConfiguratorImpl configurator) {
+		this.configurator=configurator;				
 	}
-    public boolean isValid() {
+   
+	public boolean isValid() {
+    	checker=configurator.getCompatibilityChecker();
     	for(PartType piece: config) {
     		Set<PartType> listIncompatibilities = checker.getIncompatibilities(piece);
-    		Set<PartType> listRequierements = checker.getRequirements(piece);
     		if(!listIncompatibilities.isEmpty()){
     			for(PartType p: config) {
     				if(p!=piece) {
-    					if(listIncompatibilities.contains(p))return false;
+    					if(listIncompatibilities.contains(p)) {
+    						return false;
+    					}
     				}	
     			}
     		}
+    		Set<PartType> listRequierements = checker.getRequirements(piece);
     		if(!listRequierements.isEmpty()) {
     			for(PartType p: listRequierements) {
     				if(p!=piece) {
-    					if(!config.contains(p))return false;
+    					if(!config.contains(p)) {
+    						return false;
+    					}
     				}
     			}
     		}
@@ -40,33 +44,8 @@ public class ConfigurationImpl implements Configuration {
     	return true;
     }
 
-    
     public boolean isComplete() {
-    	//modif s'arreter des que compteurX>1;
-    	int compteurEngine=0;
-    	int compteurTransmission=0;
-    	int compteurExterior=0;
-    	int compteurInterior=0;
-    	    	
-    	for(PartType piece: config) {
-    		switch(piece.getCategory().getName()) {
-	    		case"Engine":
-	    			compteurEngine++;
-	    			break;
-	    		case"Transmission":
-	    			compteurTransmission++;
-	    			break;
-	    		case"Exterior":
-	    			compteurExterior++;
-	    			break;
-	    		case"Interior":
-	    			compteurInterior++;
-	    			break;
-	    		default:
-	    			
-    		}
-    	}
-    	return (compteurEngine==1)&&(compteurTransmission==1)&&(compteurExterior==1)&&(compteurInterior==1);
+    	return config.size()==configurator.getCategories().size();	
     }
 
     public Set<PartType> getSelectedParts(){
@@ -74,29 +53,64 @@ public class ConfigurationImpl implements Configuration {
     }
 
     public void selectPart(PartType chosenPart) {
-    	config.add(chosenPart);
-    	
-    }
-
-    public PartType getSelectionForCategory(Category category) {//return la piece associee a la category donnee en parametre
-    	for(PartType piece: config) {
-    		if(piece.getCategory().getName().equals(category.getName())) return piece;
-    	}
-    	return null;
-    }
-
-    public void unselectPartType(Category categoryToClear) {
-    	Set<PartType> configRes = config;
-    	for(PartType piece: configRes) {
-    		if (piece.getCategory().equals(categoryToClear)){
-    			configRes.remove(piece);
+    	if(chosenPart != null) {
+    		
+    		if(!config.contains(chosenPart)) {
+    			Class<PartType>classePartType = PartType.class;
+    			if(!classePartType.isInstance(getSelectionForCategory(chosenPart.getCategory()))) {
+    				config.add(chosenPart);
+    			}
+    			else {
+    				throw new IllegalArgumentException("Erreur, le partType voulant être ajouté à la config"
+    						+ " ne peut pas etre ajouté car un partType de la même categorie est déjà dans la configuration");
+    			}
     		}
     	}
-    	config=configRes;
+    	else {
+    		throw new IllegalArgumentException("Erreur, le partType voulant être ajouté à la config ne peut pas etre null");
+    	}
+    }
+    
+    public PartType getSelectionForCategory(Category category) {
+    	if(category != null) {
+	    	for(PartType piece: config) {
+	    		if(piece.getCategory().getName().equals(category.getName())) {
+	    			return piece;
+	    		}
+	    	}
+	    	return null;
+	    	
+	    	
+    	}
+    	else{
+    		
+    		throw new IllegalArgumentException("Erreur, la category en parametre ne peut pas etre null");
+    		
+    	}
     }
 
-    public void clear() {//enlever toutes les pieces de la config
-    	config = new HashSet<PartType>();
-    	//config.clear();
+    public void unselectPartType(Category categoryToClear) {	
+    	if(categoryToClear != null) {
+    		ArrayList<PartType> arrayPartType = new ArrayList<>(config);
+    		int i=0;
+    		boolean breakBool = false;
+    		while(i<arrayPartType.size() && breakBool == false) {
+    			if (arrayPartType.get(i).getCategory().equals(categoryToClear)){	
+    				arrayPartType.remove(i);
+    				breakBool=true;
+        		}
+    			i++;
+        	}
+    		config = new HashSet<>(arrayPartType);
+    	}
+    	else {
+    		throw new IllegalArgumentException("Erreur, le partType voulant être enlevé de la config ne peut pas etre null");
+    	}
+    }
+    
+    @Override
+    public void clear() {
+    	config.clear();
     }
 }
+
